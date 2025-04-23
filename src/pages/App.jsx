cc7b01774d007ae7f9794a46a3786a1bbbb867ea
Ethanwang17 <ejw17@uw.edu>
2025-04-23 15:22:46 -0700

    test

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -39,7 +39,7 @@ function App() {
 						}}
 					>
 						<Title level={3} style={{color: "black", margin: 0}}>
-							Dog Adoption Search
+							TESTING
 						</Title>
 						<Button onClick={handleLogout} type="primary" danger>
 							Logout
